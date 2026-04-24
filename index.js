8916cd4ee07b10d92bfa1e429c3c16efb23e5273
bropoyo <bropoyohrnsem@gmail.com>
2026-04-24 22:05:47 +0800

    index.js

--- index.js
+++ index.js
@@ -21,4 +21,5 @@ app.post('/chat', async (req, res) => {
   res.json(data);
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
